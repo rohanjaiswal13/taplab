1ac7ee0fbeeab0d7182304c1938f38b2f2b47b41
Rohan Jaiswal <rj957977@gmail.com>
2025-11-20 13:15:50 +0530

    Sunset Chinese

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,23 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import TapLabWebsite from "./taplab_website";
+import SunsetMenu from "./sunset_chinese_menu";
+
+// ❗ No shared navbar, no shared footer, no shared links.
+// ❗ Sunset page becomes isolated completely.
 
 export default function App() {
-  return <TapLabWebsite />;
+  return (
+    <BrowserRouter>
+      <Routes>
+        {/* MAIN WEBSITE */}
+        <Route path="/" element={<TapLabWebsite />} />
+
+        {/* COMPLETELY SEPARATE PAGE */}
+        <Route path="/sunsetchinese" element={<SunsetMenu />} />
+
+        {/* IF USER TRIES TO ACCESS ANY OTHER ROUTE ON SUNSET SIDE → STILL BLOCKED */}
+        <Route path="*" element={<TapLabWebsite />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
